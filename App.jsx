2ce2ba013e417cdbe626656daddbd090370c0861
Manu007Vijay <86560296+Manu007Vijay@users.noreply.github.com>
2021-06-27 19:31:07 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+export default App;
---
+++ info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dev React JS bootcamp</p>
+    </div>
+  );
+}
+export default Info;
